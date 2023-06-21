8a6373f6e135fadfde3d746d839de5e73247a1fb
sahilverma <sahil.verma@finbox.in>
2023-06-21 18:55:03 +0530

    Added basic structure of repo

--- octo_client.go
+++ octo_client.go
@@ -1,34 +1,42 @@
 package octoclient
 
 import (
+	"errors"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/google/uuid"
 )
 
+/*
+Usage:
+  - Create instance of Octo-Client once
+  - call the service-invoke using the clientID, serviceID, payload & other params and feature
+*/
 type OctoPayload struct {
-	ClientID	uuid.UUID				`json:"client_id`
-	ServiceID	uuid.UUID				`json:"service_id`
-	Data		map[string]interface{}	`json:"data"`
+	ClientID  uuid.UUID              `json:"client_id`
+	ServiceID uuid.UUID              `json:"service_id`
+	Data      map[string]interface{} `json:"data"`
 }
 
 type OctoResponse struct {
-	Message		string					`json:"message"`
-	RequestID	uuid.UUID				`json:"request_id"`
-	Data 		map[string]interface{}	`json:"data"`
-	MetaData	map[string]interface{}	`json:"meta_data"`
+	Message   string                 `json:"message"`
+	RequestID uuid.UUID              `json:"request_id"`
+	Data      map[string]interface{} `json:"data"`
+	MetaData  map[string]interface{} `json:"meta_data"`
 }
 
 type OctoClient struct {
 	HTTPClient *http.Client
-	BaseURL			string
+	BaseURL    string
 }
 
-func New( baseUrl string) *OctoClient{
-	//TODO: trim the base url right so that it can be used with other endpoints 
+func New(baseUrl string) *OctoClient {
+	//TODO: trim the base url right so that it can be used with other endpoints
+	baseUrl = trimTrailingSlash(baseUrl)
 	return &OctoClient{
 		HTTPClient: &http.Client{},
-		BaseURL: baseUrl,
+		BaseURL:    baseUrl,
 	}
 }
 
@@ -36,24 +44,47 @@ func (o *OctoClient) getHttpClient() http.Client {
 	return *o.HTTPClient
 }
 
-func (o *OctoClient) ServiceInvoke(payload OctoPayload) {
+func (o *OctoClient) ServiceInvoke(serviceID, clientID string, payload OctoPayload) (OctoResponse, error) {
 
-	// take the /service/invoke endpoint and make new URL
+	// TODO: clientID will be replaced with token in coming future
+	apiEndpoint := "/service/invoke"
+	callingUrl := o.BaseURL + apiEndpoint
+	method := "POST"
+	var response OctoResponse
 
-	// have the method defined 
+	flag := IsValidID(serviceID) && IsValidID(clientID)
+	if !flag {
+		return response, errors.New("invalid id entered")
+	}
 
-	// convert to json 
+	finalPayload, err := ConvertStructToJSON(payload)
+	if err != nil {
+		return response, err
+	}
 
-	// verify the payload if needed 
+	req, err := http.NewRequest(method, callingUrl, finalPayload)
 
-	// call the url 
-	
-	// make the OctoResponse 
+	if err != nil {
+		return response, err
+	}
+	req.Header.Add("clientId", clientID)
+	req.Header.Add("Content-Type", "application/json")
 
-	// return the same 
+	res, err := o.HTTPClient.Do(req)
+	if err != nil {
+		return response, err
+	}
+	defer res.Body.Close()
 
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return response, err
+	}
+	// Handle if return type is HTML
+	response, err = ConvertByteToStruct(body)
+	if err != nil {
+		return response, err
+	}
+
+	return response, nil
 }
-
-// TODO: func convert to JSON
-
-// TODO: func JSON to payload 
