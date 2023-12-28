af6181898630a26ee850262d4cbea4d28896c970
Kshitiz Agrawal <kshitiz.agrawal@finbox.in>
2023-12-28 15:57:26 +0530

    feat: invoke service via file (multipart)

--- helpers.go
+++ helpers.go
@@ -2,6 +2,10 @@ package octoclient
 
 import (
 	"encoding/json"
+	"io"
+	"mime/multipart"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -25,4 +29,57 @@ func ConvertByteToStruct(body []byte) (OctoResponse, error) {
 func trimTrailingSlash(url string) string {
 	forwardSlash := "/"
 	return strings.TrimSuffix(url, forwardSlash)
+}
+
+func processTextFields(textFields []OctoTextField, multiPartWriter *multipart.Writer) error {
+	var err error
+	for _, textField := range textFields {
+		err = multiPartWriter.WriteField(textField.FieldName, textField.FieldValue)
+	}
+	return err
+}
+
+func processFileFields(fileFields []OctoFileField, multiPartWriter *multipart.Writer) error {
+	totalFileFields := len(fileFields)
+	errChannels := make([]chan error, totalFileFields)
+	for i := range errChannels {
+		errChannels[i] = make(chan error)
+	}
+
+	iteration := 0
+
+	for _, fileField := range fileFields {
+		errChan := errChannels[iteration]
+		go func(fieldName string, filePath string, errChan chan error) {
+			file, err := os.Open(filePath)
+			if err != nil {
+				errChan <- err
+				return
+			}
+			defer file.Close()
+			fileName := filepath.Base(filePath)
+
+			fileWriter, err := multiPartWriter.CreateFormFile(fieldName, fileName)
+			if err != nil {
+				errChan <- err
+				return
+			}
+
+			_, err = io.Copy(fileWriter, file)
+			if err != nil {
+				errChan <- err
+				return
+			}
+			errChan <- nil
+		}(fileField.FieldName, fileField.FilePath, errChan)
+
+		iteration++
+	}
+	for i := 0; i < totalFileFields; i++ {
+		err := <-errChannels[i]
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
