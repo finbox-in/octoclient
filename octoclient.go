c4a1f1e5803ed81bee8e8f4fa128f95b9c7ac5bf
Kshitiz Agrawal <kshitiz.agrawal@finbox.in>
2024-06-26 19:28:31 +0530

    temp: temporary fix to enable vendor switch

--- octoclient.go
+++ octoclient.go
@@ -76,8 +76,8 @@ type OctoClient struct {
 }
 
 type OctoVendorSwitch struct {
-	Enable         bool   `json:"isActive"`
-	VendorConfigID string `json:"vendorConfigId"`
+	Enable         bool      `json:"isActive"`
+	VendorConfigID uuid.UUID `json:"vendorConfigId"`
 }
 
 type Options struct {
